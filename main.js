d172ba7dd2d7f6c732fd52f3e84201f866ce3a95
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-04-27 12:58:16 -0600

    finished creating the thumbnail and collection in html to see how it would look, now i need to make them show up in the dom instead

--- main.js
+++ main.js
@@ -5,17 +5,40 @@ const btnInsert = document.getElementById("btnInsert")
 const Output = document.getElementById("Output")
 
 btnInsert.onclick = function () {
-    let URL = `https://superheroapi.com/api/${apiKey.value}/id`
+    let URL = `https://superheroapi.com/api/${apiKey.value}/489/powerstats`
     console.log(URL)
-    const encodeURL = encodeURI(URL)
-    fetch(encodeURL)
+    // const encodeURL = encodeURI(URL)
+    fetch(URL)
         .then((res) => res.json())
         // console.log(apiKey.value)
-        .then((data) => {
-            console.log(data)
+        .then((dataAPI) => {
+            const name = dataAPI[0].culmination.utc_datetime;
+            const rise = dataAPI[0].rise.utc_datetime;
+            const set = dataAPI[0].set.utc_datetime;
+            console.log(culmination, rise, set)
+            const container2 = document.querySelector('#results')
+            function createHTML(dataAPI) {
+            let html = `
+            <div class="row">
+                <div id="box">:</div>
+                <div id="box">:</div>
+                <div id="box">:</div>
+            </div>`
+                // <div class="section">
+                //     <h1>
+                //         Your Satellite Info Station
+                //     </h1>
+                //     <div class="row">
+                //     <div id="articleBoxScore">Culmination: ${culmination}</div>
+                //     <div id="articleBoxAuthor">Rise: ${rise}</div>
+                //     <div id="articleBoxComments">Set: ${set}</div>
+                //     </div>
+                // </div>`
+                        container2.innerHTML += html;
+                    }
+                    createHTML(data)
+
         })
-
-
 
     const key = inpKey.value;
     const value = inpValue.value;
@@ -25,7 +48,7 @@ btnInsert.onclick = function () {
         location.reload();
     }
 }
-
+// loop through my local storage and replace innerHTML with input
 for (i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
