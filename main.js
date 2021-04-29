a40b08fb59c78dfb1ca0d7c313f474895a32ed02
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-04-29 11:16:12 -0600

    API call pushes all objects into superheroArr. On click now filters through arr to find the characters name and displays that in the collection div.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const apiKey = document.getElementById("APIKey")
 const inpKey = document.getElementById("inpKey")
-const inpID = document.getElementById("inpID")
+const inpName = document.getElementById("inpID")
 const btnInsert = document.getElementById("btnInsert")
 const Output = document.getElementById("Output")
 let row = document.querySelector(".row")
@@ -8,66 +8,50 @@ let arr = [];
 let superheroArr = [];
 
 function APIcall() {
-    for (let i = 1; i < 732; i++) {
+    for (let i = 1; i < 5; i++) {
         let URL = `https://superheroapi.com/api/3935852106501451/${i}`
         arr.push(axios.get(URL))
-
-        // setTimeout(() => {
-
-        
-        //     fetch(URL)
-        //     .then((res) => res.json())
-        //     .then((dataAPI) => {
-        //         console.log(dataAPI)
-        //         arr.push(dataAPI)
-        //         console.log("arr equals", arr)
-        //     })
-        // }, 3000);
     }
     Promise.all(arr).then((response) =>
-        response.map(res => {superheroArr.push(res.data)
+        response.map(res => {
+            superheroArr.push(res.data)
             console.log("the console", superheroArr)
         })
     ).catch((err) => console.log(err));
 } APIcall()
 
 
-
 //loading screen to grab api
-// make the ids get grabbed on page load
-
 // make an alert or sign that can close after 20 seconds (give time for api call to work.)
+// function xForce() {
+
+// } xForce()
 
 
 
-// btnInsert.onclick = function () {
-//     let URL = `https://superheroapi.com/api/3935852106501451/search/${inpID.value}`
-//     // let URL = `https://superheroapi.com/api/3935852106501451/search/batman`
-//     console.log(URL)
-//     // const encodeURL = encodeURI(URL)
-//     fetch(URL)
-//         .then((res) => res.json())
 
-//         .then((dataAPI) => {
-//             console.log(dataAPI)
-//             for (i = 0; i < dataAPI.results.length; i++){
-//                 arr.push(dataAPI.results[i])
-//                 console.log(arr)
-//             }
-//             const charName = dataAPI.results[0].name;
-//             const charImage = dataAPI.results[0].image.url;
-//             console.log(charImage)
-//             console.log(charName)
-//             function createHTML(dataAPI) {
-//                 let html = `
-//                 <div id="box"><img id="thumbnails" src = "${charImage}">${charName}</div>`
-//                 row.innerHTML += html;
-//             }
-//             createHTML(dataAPI)
-//         })
+let charName;
+let charImage;
+btnInsert.onclick = function () {
+    for (i = 0; i < superheroArr.length;i++){
+        if (superheroArr[i].name === inpName.value){
+            console.log("hello")
+            charName = superheroArr[i].name;
+            charImage = superheroArr[i].image.url
+            console.log(charName)
+            console.log(charImage)
+        }
+    }
+    function createHTML() {
+    let html = `
+        <div id="box"><img id="thumbnails" src = "${charImage}">${charName}</div>`
+        row.innerHTML += html;
+        }
+        createHTML()
+}
 
 //     const key = inpKey.value;
-//     const value = inpID.value;
+//     const value = inpName.value;
 
 //     if (key && value) {
 //         localStorage.setItem(key, value);
@@ -90,4 +74,3 @@ function APIcall() {
 //     location.reload()
 //     console.log("hit me");
 // })
-
