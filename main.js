b7d82490b843c934631c29d091999d30286b203c
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-05-02 11:39:48 -0600

    update to creating new backside of card, adjusted css for phone, removed items from fieldset

--- main.js
+++ main.js
@@ -4,18 +4,46 @@ const inpName = document.getElementById("inpID")
 const btnInsert = document.getElementById("btnInsert")
 const Output = document.getElementById("Output")
 let row = document.querySelector(".row")
+//-----------
+//Query the card stats
+//-----------
+let species = document.getElementById("species");
+const nametoprow = document.getElementById("nametoprow");
+const durability = document.getElementById("durability")
+const combat = document.getElementById("combat")
+const intel = document.getElementById("intel")
+const power = document.getElementById("power")
+const speed = document.getElementById("speed")
+const strength = document.getElementById("strength")
+
+//-----------
+//Query the back card stats
+//-----------
+const appearance = document.querySelector(".appearance")
+const biography = document.querySelector(".biography")
+const eyecolor = document.getElementById("eyecolor")
+const gender = document.getElementById("gender")
+const haircolor = document.getElementById("haircolor")
+const height = document.getElementById("height")
+const weight = document.getElementById("weight")
+const aliases = document.getElementById("aliases")
+const alignment = document.getElementById("alignment")
+const publisher = document.getElementById("publisher")
+
+//-----------
+// API CALL arrays
+//-----------
 let arr = [];
 let superheroArr = [];
 
 function APIcall() {
-    for (let i = 1; i < 10; i++) {
+    for (let i = 1; i < 20; i++) {
         let URL = `https://superheroapi.com/api/3935852106501451/${i}`
         arr.push(axios.get(URL))
     }
     Promise.all(arr).then((response) =>
         response.map(res => {
             superheroArr.push(res.data)
-            console.log("the console", superheroArr)
         })
     ).catch((err) => console.log(err));
 } APIcall()
@@ -26,85 +54,81 @@ function APIcall() {
 // function xForce() {
 
 // } xForce()
-
-
+species;
 let charName;
 let charImage;
 let collectionArr = [];
 let storedURLs = JSON.parse(localStorage.getItem('collectionArr'));
-
-console.log(localStorage.getItem("collectionArr"))
 console.log(storedURLs)
-console.log(collectionArr)
+
+//------------
+//Call for a hero using their name.
+//------------
 
 btnInsert.onclick = function () {
     for (i = 0; i < superheroArr.length;i++){
         if (superheroArr[i].name === inpName.value){
-            console.log("hello")
+            //-----
+            // Card stats
+            //-----
+            species.innerHTML = `Race: ${superheroArr[i].appearance.race}`
+            nametoprow.innerHTML = `Name: ${superheroArr[i].name}`
+            durability.innerHTML = `Health: ${superheroArr[i].powerstats.durability}`
+            combat.innerHTML = `Combat: ${superheroArr[i].powerstats.combat}`
+            intel.innerHTML = `Intel: ${superheroArr[i].powerstats.intelligence}`
+            power.innerHTML = `Power: ${superheroArr[i].powerstats.power}`
+            speed.innerHTML =`Speed: ${superheroArr[i].powerstats.speed}`
+            strength.innerHTML = `Strength: ${superheroArr[i].powerstats.strength}`
+            //-----
+            // Back of card stats
+            //-----
+            aliases.innerHTML = `Aliases: ${superheroArr[i].biography.aliases}`;
+            alignment.innerHTML = `Alignment: ${superheroArr[i].biography.alignment}`; 
+            publisher.innerHTML = `Publisher: ${superheroArr[i].biography.publisher}`; 
+            
+            
+
+
+            //-----
+            // Image and name stats
+            //-----
             charName = superheroArr[i].name;
             charImage = superheroArr[i].image.url
+            photo.style.backgroundImage = `url(${charImage})`
             let supe = superheroArr[i];
-            
             collectionArr.push(supe)
         }
     }
+}
+//------------
+//---- set local storage
+//------------
     localStorage.setItem("collectionArr", JSON.stringify(collectionArr))
     localStorage.setItem("name", charName )
     localStorage.setItem("image", charImage )
     createHTML(charImage, charName)
-}
-
+    console.log(collectionArr)
 //------------
 // Create DOM element for collection
 //------------
 function createHTML(img, name) {
-    let html = `
-    <div id="box"><img id="thumbnails" onclick="clickedThumb()" src = "${img}">${name}</div>`
+    let html = `<div id="box"><img id="thumbnails" src = "${img}">${name}</div>`
     row.innerHTML += html;
 }
+
 if(storedURLs !== null){
     collectionArr = JSON.parse(localStorage.getItem('collectionArr')).map(x => x);
     for (i=0; i < storedURLs.length; i++){
         createHTML(storedURLs[i].image.url, storedURLs[i].name)
-        console.log(storedURLs[i].name)
     }
 }
 
-//------------
-// Click on an image to get the card 
-//------------
-const thumbnails = document.querySelector("thumbnails")
-const photo = document.getElementById("photo")
 
-function clickedThumb(){
-    photo.style.backgroundImage = `url(${charImage})`
-    console.log("photo clicked")
-}
-clickedThumb()
+// clearing the local storage and the history
+const btnClear = document.getElementById("btnClear");
 
-
-
-//     const key = inpKey.value;
-//     const value = inpName.value;
-
-//     if (key && value) {
-//         localStorage.setItem(key, value);
-//     }
-// }
-
-// // loop through my local storage and replace innerHTML with input
-// for (i = 0; i < localStorage.length; i++) {
-//     const key = localStorage.key(i);
-//     const value = localStorage.getItem(key);
-//     Output.innerHTML += `${key}: ${value} <br />`
-//     // location.reload()
-// }
-
-// // clearing the local storage and the history
-// const btnClear = document.getElementById("btnClear");
-
-// btnClear.addEventListener('click', function () {
-//     localStorage.clear();
-//     location.reload()
-//     console.log("hit me");
-// })
+btnClear.addEventListener('click', function () {
+    localStorage.clear();
+    location.reload()
+    console.log("hit me");
+})
